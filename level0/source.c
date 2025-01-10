bfc0be754be3ab9bfb4146771eb1bc46de2f2e9e
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-01-10 12:03:45 +0100

    clean level0 source.c

--- level0/source.c
+++ level0/source.c
@@ -1,16 +1,29 @@
-int32_t main(void* arg1) {
-	if (atoi(*(uint32_t*)((char*)arg1 + 4)) != 0x1a7)
-		_IO_fwrite("No !\n", 1, 5, _IO_stderr);
-	else
-	{
-		char* var_20 = __strdup("/bin/sh");
-		int32_t var_1c_1 = 0;
-		int32_t eax_5 = __getegid();
-		int32_t eax_6 = __geteuid();
-		__setresgid(eax_5, eax_5, eax_5);
-		__setresuid(eax_6, eax_6, eax_6);
-		execv("/bin/sh", &var_20);
-	}
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
 
-	return 0;
+int main(int ac, char** av) {
+  int nb;
+  char* args[2];
+  int local_1c;
+  uid_t uid;
+  gid_t gid;
+
+  nb = atoi(av[1]);
+  if (nb == 423) {
+    args[0] = strdup("/bin/sh");
+    args[1] = NULL;
+
+    gid = getegid();
+    uid = geteuid();
+    setresgid(gid, gid, gid);
+    setresuid(uid, uid, uid);
+    execv("/bin/sh",args);
+  }
+  else {
+    fwrite("No !\n",1,5,(FILE *)stderr);
+  }
+  return 0;
 }
