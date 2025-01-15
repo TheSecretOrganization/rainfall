5c1d00f13b26c7a60b74099b3ea5df2e68a85fe2
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-01-15 18:07:59 +0100

    level2 done

--- level2/source.c
+++ level2/source.c
@@ -2,17 +2,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 void p(void) {
-	uint unaff_retaddr;
 	char buff[76];
+	uint unaff_retaddr;
 
 	fflush(stdout);
 	gets(buff);
 	if ((unaff_retaddr & 0) == 0) {
 		printf("(%p)\n", unaff_retaddr);
 		/* WARNING: Subroutine does not return */
-		exit(1);
+		_exit(1);
 	}
 	puts(buff);
 	strdup(buff);
